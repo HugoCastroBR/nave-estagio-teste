1081a25e6d5cb99998cc78d24372bacd31ce1778
Hugo Castro <61667328+HugoCastroBR@users.noreply.github.com>
2021-03-07 13:49:35 -0300

    :p

--- Bonus/structure/database/connect.js
+++ Bonus/structure/database/connect.js
@@ -5,7 +5,7 @@ const connect = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '32119294h',
+    password: {password :p},
     database: 'bonus' 
 })
 
